c0fc0291a6ed3af2f3e43783bfe3302a1d95f9e0
Manu Gonzalez <jmgonzalez@ces.vocento.com>
2019-07-05 08:53:49 +0200

    Unique but not insane filenames chapter

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Sluggable\Util\Urlizer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
@@ -74,9 +75,11 @@ class ArticleAdminController extends BaseController
     {
         /** @var UploadedFile $uploadedFile */
         $uploadedFile = $request->files->get('image');
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid('', true).'.'.$uploadedFile->guessExtension();
 
         $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
-        dd($uploadedFile->move($destination));
+        dd($uploadedFile->move($destination, $newFilename));
     }
 
     /**
